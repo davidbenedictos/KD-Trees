b11c0c79443388e968a39ba30a6785318abf2e9e
paumartinezrs <97336651+paumartinezrs@users.noreply.github.com>
2023-10-04 13:20:08 +0200

    Update kdtree.hh

--- kdtree.hh
+++ kdtree.hh
@@ -9,13 +9,13 @@
 typedef std::vector<float> Point;
 
 
-struct KDTreeNode
+struct KDTreePoint
 {
         Point point;
         KDTreeNode* left;
         KDTreeNode* right;
        
-        KDTreeNode(const Point& p) : point(p), left(nullptr), right(nullptr));
+        KDTreePoint(const Point& p) : point(p), left(nullptr), right(nullptr));
 };
 
 class KDTree {
