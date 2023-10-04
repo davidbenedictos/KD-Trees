daba25ced434f26057c9986e4a0a76cabc45ba2f
davidbenedictos <83397735+davidbenedictos@users.noreply.github.com>
2023-10-04 14:30:43 +0200

    Update kdtree.cc

--- kdtree.cc
+++ kdtree.cc
@@ -7,39 +7,40 @@ KDTree::KDTree() : root(nullptr) {
 }
 
 
-KDTree::KDTree(std::vector<Point>& points){
-    root = buildTree(points, 0);
+KDTree::KDTree(std::vector<Point>& points) : root(nullptr){  
+    for(int i = 0; i < points.size(); ++i){
+      insertPoint(root, points[i], 0);
+    }
 } 
 
-void KDTree::insertPoint(Point point){
-    //Ya lo haremos
+KDTreeNode* KDTree::createNode(Point p){
+    KDTreeNode* node = new KDTreeNode;
+        node->point = p;
+        node->left = node->right = nullptr;
 }
 
-KDTreeNode* KDTree::buildTree(std::vector<Point>& points, int depth) {
-        if (points.empty()) {
-            return nullptr;
-        }
-
-        int k = points[0].coordinates.size();
-        int axis = depth % k;
-        
-        // Sort points and choose median as pivot element
-        std::sort(points.begin(), points.end(), [axis](const Point& a, const Point& b) {
-            return a.coordinates[axis] < b.coordinates[axis];
-        });
-
-        int median = points.size() / 2;
-
-        // Create node and construct subtrees
-        KDTreeNode* node = new KDTreeNode(points[median]);
-        node->left = buildTree(std::vector<Point>(points.begin(), points.begin() + median), depth + 1);
-        node->right = buildTree(std::vector<Point>(points.begin() + median + 1, points.end()), depth + 1);
-
+KDTreeNode* KDTree::insertPoint(KDTreeNode* r, Point p, int depth){
+    
+    KDTreeNode* node = createNode(p);
+    
+    if (root == nullptr){       
         return node;
+    } else {
+        int k = p.size();
+        int i = depth%k;
+        if(p[i] < r->point[i]){
+            r->left = insertPoint(r->left, p, depth + 1);
+        } else {
+             r->right = insertPoint(r->right, p, depth + 1);
+        }
     }
+    return r;
+}
 
 
-    ~KDTree() {
+
+
+~KDTree() {
         // Implementa la eliminación de nodos y liberación de memoria aquí.
     }
 
