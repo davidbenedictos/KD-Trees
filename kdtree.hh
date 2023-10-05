2acf7d5e654f396cca3bb20f3fc1eea636ddbfa5
Jialiang Li <138883103+jiialiang@users.noreply.github.com>
2023-10-05 09:10:34 +0200

    Update kdtree.hh

--- kdtree.hh
+++ kdtree.hh
@@ -1,5 +1,5 @@
-#ifndef kdtree.hh
-#define kdtree.hh
+#ifndef __KDTREE_HH__
+#define __KDTREE_HH__
 
 #include <iostream>
 #include <vector>
@@ -8,39 +8,48 @@
 
 typedef std::vector<float> Point;
 
-class KDTree {
-
-private:
-    struct KDTreeNode
+struct KDTreeNode
     {
         Point point;
         KDTreeNode* left;
         KDTreeNode* right;     
     };
 
+class KDTree {
+
+private:
+
     KDTreeNode* root;
 
-    KDTreeNode* buildTree(std::vector<Point>& points, int depth);
+    //KDTreeNode* buildTree(std::vector<Point>& points, int depth);
 
-    KDTreeNode* createNode(Point);
+    KDTreeNode* createNode(const Point& p);
 
     void destroyTree(KDTreeNode* node);
+
+    void printTree(KDTreeNode* node, int depth);
   
 public:
     //Constructor arbol vacio
     KDTree();
 
     //Constructor con n puntos k-dimensionales
-    KDTree(std::vector<Point> p);
+    KDTree(std::vector<Point>& points);
 
     //Constructor con n puntos k-dimensionales aleatorios
     KDTree(int N, int K);
 
     //Metodo para añadir una clave k-dimensional al árbol
-    KDTreeNode* insertPoint(KDTreeNode* r, Point p, int depth);
+    KDTreeNode* insertPoint(KDTreeNode* r, const Point& p, int depth);
 
     // Función de búsqueda de un punto en el árbol.
     //search(const Point& target);
+
+    // Función para imprimir el árbol.
+    void print();
+
+    // Función para un punto en el árbol.
+    void insert(const Point& p);
 
     // Destructor para liberar la memoria.
     ~KDTree();
